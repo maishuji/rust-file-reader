6b100466263f6de45ff9a2be3c78f497aeca62bd
Quentin Cartier <daoliangshumaishuji@gmail.com>
2024-12-04 15:14:31 +0000

    Make web server async

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 use std::fs;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use async_std::net::TcpListener;
+use async_std::net::TcpStream;
+use async_std::prelude::*;
+use futures::stream::StreamExt;
+use async_std::task::spawn;
 
-fn main() {
+#[async_std::main]
+async fn main() {
     // Listen for incoming TCP connections on localhost port 7878
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    // Block forever, handling each request that arrives at this IP address
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
+    listener
+    .incoming()
+    .for_each_concurrent(/* limit */ None, |tcpstream| async move {
+        let tcpstream = tcpstream.unwrap();
+        spawn(handle_connection(tcpstream));
+    })
+    .await;
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    // Read the first 1024 bytes of data from the stream
+async fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
 
@@ -34,6 +36,6 @@ fn handle_connection(mut stream: TcpStream) {
     // Write response back to the stream,
     // and flush the stream to ensure the response is sent back to the client
     let response = format!("{status_line}{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
